c4052db09df90b10e4f9e28a4b87e28458172418
Arash Amiry <ArashAmiry@hotmail.com>
2024-03-03 14:09:51 +0100

    text padding homepage

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -5,7 +5,7 @@ function Home(){
     return(
         <Container fluid className='px-0 home-image'>
             <Row className='row-home'>
-                <Col md={12} className="home-name text-start py-5 px-5">
+                <Col md={12} className="home-name text-start">
                     <h1><strong>YUNG LEAN</strong></h1>
                 </Col>
             </Row>
