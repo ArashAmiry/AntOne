c5baf2ce238c4fb9f7e8660fb6eee156bb9e336a
Arash Amiry <ArashAmiry@hotmail.com>
2024-03-03 18:34:27 +0100

    play-buttons added and on correct position

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,12 +1,34 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import './Home.css';
 
-function Home(){
-    return(
+function Home() {
+    return (
         <Container fluid className='px-0 home-image'>
             <Row className='row-home'>
                 <Col md={12} className="home-name text-start">
                     <h1><strong>YUNG LEAN</strong></h1>
+                </Col>
+            </Row>
+
+            <Row className='music-player'>
+                <Col md={1} className='button'>
+                    <h2><strong>|&lt;</strong></h2>
+                </Col>
+
+                <Col md={1} className='button'>
+                    <h2><strong>||</strong></h2>
+                </Col>
+
+                <Col md={1} className='button'>
+                    <h2><strong>&gt;|</strong></h2>
+                </Col>
+
+                <Col>
+                
+                </Col>
+
+                <Col>
+                
                 </Col>
             </Row>
         </Container>
